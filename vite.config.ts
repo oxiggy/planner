b051d4f13bdd5b8a90feb69ab2abb0dce7b8fe50
Ada Void <adavoid@proton.me>
2026-05-22 02:01:39 +0400

    feat: add GitHub Actions workflow for deploying to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'node:path'
 
+const BASE_PATH = process.env.BASE_PATH
+
 export default defineConfig({
+  base: BASE_PATH ? `${BASE_PATH}/` : '/',
   plugins: [react()],
   resolve: {
     alias: {
